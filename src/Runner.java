fe1d1437b892c8b1327e3964db487a894b1a46cd
rezik <rezikun@gmail.com>
2021-03-17 22:56:13 +0200

    debug

--- src/Runner.java
+++ src/Runner.java
@@ -12,11 +12,13 @@ public class Runner implements AM {
         System.out.println(String.format("Got row of size %d", row.size()));
         List<Cell> newRow = new ArrayList<>();
         for (Cell cell : row) {
+            System.out.println(String.format("Before %d", cell.matrix[0][0].getBlue()));
             float[][] rMatrix = new float[8][8];
             float[][] gMatrix = new float[8][8];
             float[][] bMatrix = new float[8][8];
             cell.transform(rMatrix, gMatrix, bMatrix);
             cell = Cell.detransform(rMatrix, gMatrix, bMatrix);
+            System.out.println(String.format("After %d", cell.matrix[0][0].getBlue());
             newRow.add(cell);
         }
         info.parent.write((Serializable) newRow);
