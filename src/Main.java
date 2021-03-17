2b5c6bf63f1989e07def1d52fac096056b42e809
rezik <rezikun@gmail.com>
2021-03-18 00:01:53 +0200

    debug

--- src/Main.java
+++ src/Main.java
@@ -17,21 +17,9 @@ public class Main {
         System.out.println("Loading image...");
         File img = new File(curtask.findFile("image.jpg"));
         BufferedImage newImage = ImageIO.read(img);
-        System.out.println("OLD");
-        for (int i = 0; i < 16; ++i) {
-            for (int j = 0; j < 16; ++j){
-                Color color = new Color(newImage.getRGB(i, j));
-                System.out.println(String.format("A: %d, R: %d G: %d B: %d",
-                        color.getAlpha(), color.getRed(), color.getGreen(), color.getBlue()));
-            }
-        }
-        System.out.println("\n");
         ImageMatrix imageMatrix = new ImageMatrix(newImage);
 
         List<List<Cell>> cells = imageMatrix.splitIntoCells();
-        BufferedImage processedTest = ImageMatrix.fromCells(cells).toBufferedImage();
-        File outputfileTest = new File("processedImageTest.jpg");
-        ImageIO.write(processedTest, "jpg", outputfileTest);
 
         List<channel> channels = new ArrayList<>();
         for (List<Cell> cellRow: cells) {
@@ -52,15 +40,6 @@ public class Main {
 
         System.out.println("Decoding image.");
         BufferedImage processed = ImageMatrix.fromCells(processedCells).toBufferedImage();
-        System.out.println("NEW");
-        for (int i = 0; i < 16; ++i) {
-            for (int j = 0; j < 16; ++j){
-                Color color = new Color(processed.getRGB(i, j));
-                System.out.println(String.format("A: %d, R: %d G: %d B: %d",
-                        color.getAlpha(), color.getRed(), color.getGreen(), color.getBlue()));
-            }
-        }
-        System.out.println("\n");
         File outputfile = new File("processedImage.jpg");
         ImageIO.write(processed, "jpg", outputfile);
         System.out.println("Done.");
--- src/Runner.java
+++ src/Runner.java
@@ -9,16 +9,13 @@ public class Runner implements AM {
     @Override
     public void run(AMInfo info) {
         List<Cell> row = (List<Cell>) info.parent.readObject();
-        System.out.println(String.format("Got row of size %d", row.size()));
         List<Cell> newRow = new ArrayList<>();
         for (Cell cell : row) {
-            System.out.println(String.format("Before %d", cell.matrix[0][0].getRGB()));
             float[][] rMatrix = new float[8][8];
             float[][] gMatrix = new float[8][8];
             float[][] bMatrix = new float[8][8];
             cell.transform(rMatrix, gMatrix, bMatrix);
             cell = Cell.detransform(rMatrix, gMatrix, bMatrix);
-            System.out.println(String.format("After  %d\n", cell.matrix[0][0].getRGB()));
             newRow.add(cell);
         }
         info.parent.write((Serializable) newRow);
