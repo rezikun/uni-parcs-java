5b8e00663bea6ff4de64b924aa5cb4a2c762d747
rezik <rezikun@gmail.com>
2021-03-18 13:33:51 +0200

    debug

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         AMInfo info = new AMInfo(curtask, null);
 
         System.out.println("Loading image...");
-        File img = new File(curtask.findFile("huge.jfif"));
+        File img = new File(curtask.findFile("huge.jpg"));
         BufferedImage newImage = ImageIO.read(img);
         ImageMatrix imageMatrix = new ImageMatrix(newImage);
 
